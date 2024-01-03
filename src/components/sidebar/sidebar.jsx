6e3b87ff9335dc539280cbd2800226784f7429b2
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-01-03 13:00:50 +0530

    erferf

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,95 +1,186 @@
-import React from 'react'
+import React from 'react';
 import './sidebar.css';
 import { NavLink } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
-import swal from 'sweetalert'
+import { useNavigate } from 'react-router-dom';
+import swal from 'sweetalert';
 import GrassIcon from '@mui/icons-material/Grass';
-import {toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { header } from '../../store/login';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Sidebar = () => {
     let navigate = useNavigate();
     const dispatch = useDispatch();
     const log = useSelector((state) => state.login);
     const useralldetail = useSelector((state) => state.userexplist);
-    const linke = [{
-        name: "Dashboard",
-        link: '/',
-        logo: "fa fa-tachometer"
-    }, {
-        name: "Expenses",
-        link: '/addexpense',
-        logo: "fa fa-university"
-    }, {
-        name: "Data Alaysis",
-        link: '/datanalysis',
-        logo: "fa fa-book"
-    }, {
-        name: "Report",
-        link: '/report',
-        logo: "fa fa-cloud-download"
-    },
-    {
-        name: "Print",
-        link: '/print',
-        logo: "fa fa-print"
-    }
-    ]
+
+    const [adminSubMenuVisible, setAdminSubMenuVisible] = React.useState(false);
+
+    const toggleAdminSubMenu = () => {
+        setAdminSubMenuVisible(!adminSubMenuVisible);
+    };
+
+    const linke = [
+        {
+            name: 'Dashboard',
+            link: '/',
+            logo: 'fa fa-tachometer',
+        },
+        {
+            name: 'Expenses',
+            link: '/addexpense',
+            logo: 'fa fa-university',
+        },
+        {
+            name: 'Data Alaysis',
+            link: '/datanalysis',
+            logo: 'fa fa-book',
+        },
+        {
+            name: 'Report',
+            link: '/report',
+            logo: 'fa fa-cloud-download',
+        },
+        {
+            name: 'Print',
+            link: '/print',
+            logo: 'fa fa-print',
+        },
+    ];
+
     const fr = () => {
         swal({
-            title: "Are you sure to Logout?",
-            text: "",
-            icon: "warning",
+            title: 'Are you sure to Logout?',
+            text: '',
+            icon: 'warning',
             buttons: true,
             dangerMode: true,
         }).then(async (willDelete) => {
             if (willDelete) {
-                dispatch(header("Login"))
-                toast.success("Logout successfull", { autoClose: 1300 })
+                dispatch(header('Login'));
+                toast.success('Logout successful', { autoClose: 1300 });
                 navigate('/logout');
-            } else {
-                // swal("Your data is safe!");
             }
         });
-    }
+    };
+
     return (
         <>
-            <div className={log.narrow ? "sidebar narrow" : "sidebar"}>
+            <div className={log.narrow ? 'sidebar narrow' : 'sidebar'}>
                 <div className="clogo">
-                    <NavLink className={(navData) => (navData.isActive ? 'active' : '')} style={{ textDecoration: 'none' }} to='/' > <span className="li" ><span className="logo"> <GrassIcon className='company' /></span><span className="name">Accusoft</span></span></NavLink>
+                    <NavLink
+                        className={(navData) => (navData.isActive ? 'active' : '')}
+                        style={{ textDecoration: 'none' }}
+                        to="/"
+                    >
+                        {' '}
+                        <span className="li">
+                            <span className="logo">
+                                {' '}
+                                <GrassIcon className="company" />
+                            </span>
+                            <span className="name">Accusoft</span>
+                        </span>
+                    </NavLink>
                 </div>
                 <div className="link">
-                    {log.islogin ? linke.map((val, ind) => {
-                        return (
-                                <NavLink key={ind} className={(navData) => (navData.isActive ? 'active' : '')} style={{ textDecoration: 'none' }} to={val.link} >
-                                    <span className="li" onClick={() =>  dispatch(header(val.name))}>
+                    {log.islogin
+                        ? linke.map((val, ind) => {
+                            return (
+                                <NavLink
+                                    key={ind}
+                                    className={(navData) => (navData.isActive ? 'active' : '')}
+                                    style={{ textDecoration: 'none' }}
+                                    to={val.link}
+                                >
+                                    <span className="li" onClick={() => dispatch(header(val.name))}>
                                         <span className="logo">
                                             <i title={val.name} className={val.logo} aria-hidden="true"></i>
                                         </span>
                                         <span className="name">{val.name}</span>
                                     </span>
                                 </NavLink>
-                        )
-                    }) : null}
-                    {log.islogin ? useralldetail.user.isadmin ?
-                        <NavLink  className={(navData) => (navData.isActive ? 'active' : '')} style={{ textDecoration: 'none' }} to="/admin" >
-                            <span className="li" onClick={() => dispatch(header("Admin"))}>
+                            );
+                        })
+                        : null}
+                    {log.islogin ? useralldetail.user.isadmin ? (
+                        <div className="admin-panel">
+                            <span className={`li ${adminSubMenuVisible ? 'active' : ''}`} onClick={toggleAdminSubMenu} >
                                 <span className="logo">
-                                    <i title={"Admin"} className="fa fa-lock" aria-hidden="true"></i>
+                                    <i title={'Admin'} className="fa fa-lock" aria-hidden="true"></i>
                                 </span>
                                 <span className="name">Admin</span>
+                                <span><i className={adminSubMenuVisible ? "fa fa-arrow-up":"fa fa-arrow-down"} aria-hidden="true"></i></span>
                             </span>
-                        </NavLink> : null : null}
-
-                    {log.islogin ? <span className="li" onClick={fr}><span className="logo"><i title='Sign Out' className="fa fa-sign-out" aria-hidden="true"></i></span><span className="name">Logout</span></span> : <NavLink className={(navData) => (navData.isActive ? 'active' : '')} style={{ textDecoration: 'none' }} to="/login" > <span className="li" onClick={() => dispatch(header("Login"))}><span className="logo"><i title='Sign In' className="fa fa-user" aria-hidden="true"></i></span><span className="name">Login</span></span></NavLink>}
-
+                            {adminSubMenuVisible && (
+                                <div className="sub-menu">
+                                    <NavLink
+                                        className={(navData) => (navData.isActive ? 'active' : '')}
+                                        style={{ textDecoration: 'none' }}
+                                        to="/admin/users"
+                                    >
+                                        <span className="li" onClick={() => dispatch(header('Users'))}>
+                                            <span className="logo">
+                                                <i title={'Users'} className="fa fa-user" aria-hidden="true"></i>
+                                            </span>
+                                            <span className="name">Users</span>
+                                        </span>
+                                    </NavLink>
+                                    <NavLink
+                                        className={(navData) => (navData.isActive ? 'active' : '')}
+                                        style={{ textDecoration: 'none' }}
+                                        to="/admin/expenses"
+                                    >
+                                        <span className="li" onClick={() => dispatch(header('Expenses'))}>
+                                            <span className="logo">
+                                                <i title={'Expenses'} className="fa fa-university" aria-hidden="true"></i>
+                                            </span>
+                                            <span className="name">Expenses</span>
+                                        </span>
+                                    </NavLink>
+                                    <NavLink
+                                        className={(navData) => (navData.isActive ? 'active' : '')}
+                                        style={{ textDecoration: 'none' }}
+                                        to="/admin/reporting"
+                                    >
+                                        <span className="li" onClick={() => dispatch(header('Reporting'))}>
+                                            <span className="logo">
+                                                <i title={'Reporting'} className="fa fa-cloud-download" aria-hidden="true"></i>
+                                            </span>
+                                            <span className="name">Reporting</span>
+                                        </span>
+                                    </NavLink>
+                                </div>
+                            )}
+                        </div>
+                    ) : null:null}
+                    {log.islogin ? (
+                        <span className="li" onClick={fr}>
+                            <span className="logo">
+                                <i title="Sign Out" className="fa fa-sign-out" aria-hidden="true"></i>
+                            </span>
+                            <span className="name">Logout</span>
+                        </span>
+                    ) : (
+                        <NavLink
+                            className={(navData) => (navData.isActive ? 'active' : '')}
+                            style={{ textDecoration: 'none' }}
+                            to="/login"
+                        >
+                            {' '}
+                            <span className="li" onClick={() => dispatch(header('Login'))}>
+                                <span className="logo">
+                                    <i title="Sign In" className="fa fa-user" aria-hidden="true"></i>
+                                </span>
+                                <span className="name">Login</span>
+                            </span>
+                        </NavLink>
+                    )}
                 </div>
-
             </div>
         </>
-    )
-}
+    );
+};
 
 export default Sidebar;

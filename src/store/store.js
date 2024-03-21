f697e50242a6e54dc0a05d7f8e3a82804628c294
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-03-21 16:13:56 +0530

    dfvdf

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,7 @@ import {persistReducer} from 'redux-persist';
 import { combineReducers } from "@reduxjs/toolkit";
 
 const persistConfig={
-    key:"expebvnse",
+    key:"Accosoft",
     version:1,
     storage:storage
 }

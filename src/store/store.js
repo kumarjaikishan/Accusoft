0bf2d2b60e92b15f90f996b826b88025a7818876
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-04-26 12:24:27 +0530

    xcvcv

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,7 @@ import {persistReducer} from 'redux-persist';
 import { combineReducers } from "@reduxjs/toolkit";
 
 const persistConfig={
-    key:"Accosoften",
+    key:"Accosofte",
     version:1,
     storage:storage
 }

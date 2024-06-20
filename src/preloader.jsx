7e1d8ff78f6b39ab55a35a4ef01aeacec5083478
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-06-20 17:49:58 +0530

    dfrtg

--- src/preloader.jsx
+++ src/preloader.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Lottie from 'react-lottie';
-import preloaderData from './assets/wallet.json';
 
 const Preloader = () => {
+  const [pre, setpre] = useState('');
+  const url = 'https://res.cloudinary.com/dusxlxlvm/raw/upload/v1718885399/accusoft/assets/preloader/wallet_iuwpoz.json'
+  const whiteurl = 'https://res.cloudinary.com/dusxlxlvm/raw/upload/v1718885399/accusoft/assets/preloader/walletwhite_wcfo1j.json'
+  useEffect(() => {
+    let modee = localStorage.getItem('mode');
+    if (modee == 'true') {
+      fetch(whiteurl)
+        .then(response => response.json())
+        .then(data => setpre(data))
+        .catch(error => console.error('Error fetching animation:', error));
+    } else {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => setpre(data))
+        .catch(error => console.error('Error fetching animation:', error));
+    }
+
+  }, [])
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    animationData: preloaderData,
+    animationData: pre,
   };
 
   return (

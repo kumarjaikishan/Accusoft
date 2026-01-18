5d87022bc24931cff7582ea42aa6d4a566ff2021
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2026-01-18 12:23:20 +0530

    sdfdsf

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react";
+import viteCompression from 'vite-plugin-compression';
 import svgrPlugin from "vite-plugin-svgr";
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
   build: {
     outDir: "build",
   },
@@ -18,9 +18,25 @@ export default defineConfig({
     svgrPlugin({
       svgrOptions: {
         icon: true,
-     },
+      },
     }),
+
+    // Brotli (best)
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      deleteOriginFile: false,
+      filter: (file) => /\.(js|css|html|svg)$/.test(file),
+      threshold: 1024
+    }),
+
+    // Gzip (fallback)
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      deleteOriginFile: false,
+      filter: (file) => /\.(js|css|html|svg)$/.test(file),
+      threshold: 1024
+    })
   ],
-  
-  
 });

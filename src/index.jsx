8ef9acdac2b3664d2784b4bdbb4004e3d3730220
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2026-01-27 13:05:12 +0530

    dffd

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let persistor = persistStore(store);
